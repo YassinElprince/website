90be1fecde618a7afe265802d5ac80283d25d798
YassinElprince <yassin.elprince2005@gmail.com>
2025-12-06 21:28:28 +0200

    Add files via upload

---
+++ models/menu.js
@@ -0,0 +1,19 @@
+// simple mock menu data
+const menus = [
+    {
+        id: 1,
+        restaurantId: 1,
+        name: "Margherita Pizza",
+        price: 120,
+        img: "margherita.jpg"
+    },
+    {
+        id: 2,
+        restaurantId: 1,
+        name: "Pepperoni Pizza",
+        price: 150,
+        img: "pepperoni.jpg"
+    }
+];
+
+module.exports = { menus };
---
+++ models/order.js
@@ -0,0 +1,12 @@
+// simple mock order data
+const orders = [
+    {
+        id: 1,
+        customerEmail: "test@example.com",
+        restaurantId: 1,
+        items: "Margherita Pizza",
+        totalPrice: 120,
+    }
+];
+
+module.exports = { orders };
