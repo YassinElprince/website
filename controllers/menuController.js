967fbaa8c798018c41f038290904e4f32d0d660c
YassinElprince <yassin.elprince2005@gmail.com>
2025-12-13 21:55:47 +0200

    Update menuController.js

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,51 +1,53 @@
-const { menus } = require('../models/menu');
+const { db } = require('../db.js');
 
 // Retrieve all menu items
 const retrieveAllMenu = (req, res) => {
-    const allmenus = menus;
+  db.all("SELECT * FROM MENU", [], (err, rows) => {
+    if (err) return res.status(500).send("Database error.");
+
     res.status(200).json({
-        status: 'success',
-        message: 'Menu retrieved successfully',
-        results: allmenus.length,
-        data: allmenus,
+      status: 'success',
+      message: 'Menu retrieved successfully',
+      results: rows.length,
+      data: rows,
     });
+  });
 };
 
-// Create a new menu item
+// Create a new Menu
 const createMenu = (req, res) => {
-    const { 
-        restaurantId,
-        name, 
-        price, 
-        img,
-    } = req.body;
+    const {name,price, restaurant_id} = req.body;
+    
+    db.get("SELECT * FROM RESTAURANT WHERE ID = ?",
+        [restaurant_id],
+        (err, row) => {
+            if (err) return res.status(500).send("Database error.");
 
-    if (
-        !restaurantId ||
-        !name || 
-        !price
-    ) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Please provide required fields.',
-        });
-    } 
+            if (!row) {
+                return res.status(403).json({ message: "Not allowed" });
+            }
+            // Basic validation
+            if (!name || !price) {
+                return res.status(400).json({ message: 'All fields are required' });
+            }
+            
+                // Insert
+            const query = `
+                INSERT INTO MENU (NAME, PRICE, RESTAURANT_ID)
+                VALUES (?, ?, ?)
+            `;
+            
+            const params = [name,price,restaurant_id];
+            db.run(query, params, (err) => {
+                if (err) {
+                    return res.status(500).send('Database error.');
+                }
 
-    const newMenuItem = {
-        id: menus.length + 1,
-        restaurantId,
-        name,
-        price,
-        img,
-    };
-
-    menus.push(newMenuItem);
-
-    res.status(201).json({
-        status: 'success',
-        message: 'Menu item created successfully',
-        data: newMenuItem,
-    });
+                res.status(201).json({status: 'success', message: 'menu created successfully', data: this.lastID,
+                });
+            });
+        }
+    );
 };
 
 module.exports = { 
